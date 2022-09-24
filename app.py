84f30d8e9474ce978d10e62db788ab943a77d66f
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-24 13:59:58 -0600

    Added the webbrowser function

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import webbrowser
 import wikipedia
 
 try:
     city = wikipedia.page(input("Which city do you wanna see?\n"))
     print(city.title)
     print(city.content)
+    webbrowser.open(city.url)
 except wikipedia.exceptions.PageError:
     print("This city is invalid. \nTry again.")
 except:
